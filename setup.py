2ecc95dbbd4dc3d8b11c20e59f6aad1b51e753db
Ryan Kelly <ryan@ryankelly.us>
2021-12-15 22:05:31 -0500

    v1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="locald",
-    version="1.3",
+    version="1.4",
     description="run local services/microservices for development",
     author="Ryan P. Kelly",
     author_email="ryan@ryankelly.us",
